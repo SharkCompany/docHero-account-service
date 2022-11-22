cac19f0c2442e37465b93a98fc3d26cdf7855163
phongvanngo <19520834@gm.uit.edu.vn>
2022-11-22 15:29:47 +0700

    update account service

---
+++ account-service/src/main/java/com/dochero/accountservice/controller/AccountController.java
@@ -0,0 +1,14 @@
+package com.dochero.accountservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountController {
+    @GetMapping("/ping")
+    public String helloWorld() {
+        return "hello worold";
+    }
+}
