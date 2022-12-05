b3d448b030bfa3df6865d6c453671bf7e9b34c9b
phongvanngo <19520834@gm.uit.edu.vn>
2022-12-05 19:40:37 +0700

    update

--- account-service/src/main/java/com/dochero/accountservice/_draft/ResponseErrorMessage.java
+++ account-service/src/main/java/com/dochero/accountservice/_draft/ResponseErrorMessage.java
@@ -4,7 +4,7 @@ public enum ResponseErrorMessage {
   DUPLICATED_EMAIL("Email already exists"),
   EMPTY_PASSWORD("Password cannot be empty"),
   EMPTY_FULL_NAME("FullName cannot be empty"),
-  INVALID_EMAIL("Invalid Email"),
+  INVALID_EMAIL("Invalid Email "),
   ACCOUNT_NOT_FOUND("Account does not exist")
   ;
 
