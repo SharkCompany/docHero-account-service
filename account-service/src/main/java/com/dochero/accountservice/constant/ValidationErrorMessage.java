114248f5b6512a7acc5c8722ba98029ce87119f1
phongvanngo <19520834@gm.uit.edu.vn>
2022-12-05 19:15:01 +0700

    update

--- account-service/src/main/java/com/dochero/accountservice/constant/ValidationErrorMessage.java
+++ account-service/src/main/java/com/dochero/accountservice/constant/ValidationErrorMessage.java
@@ -4,7 +4,6 @@ public interface ValidationErrorMessage {
 
    String EMPTY_PASSWORD = "Password cannot be empty";
    String EMPTY_FULLNAME = "Fullname cannot be empty";
-   String DUPLICATE_EMAIL = "Email already exists";
    String INVALID_EMAIL = "Invalid Email";
    String ACCOUNT_NOT_FOUND = "Account Not Found";
 
