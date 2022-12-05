f93e0b25cd9117b944915e50922748c95eb49496
phongvanngo <19520834@gm.uit.edu.vn>
2022-12-05 23:49:42 +0700

    update pom file

--- account-service/src/main/java/com/dochero/accountservice/controller/CheckConnectionController.java
+++ account-service/src/main/java/com/dochero/accountservice/controller/CheckConnectionController.java
@@ -1,5 +1,6 @@
 package com.dochero.accountservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 public class CheckConnectionController {
+
+  @Operation(summary = "Ping To Service")
   @GetMapping("/")
   public String homepage() {
     return "Welcome To Account Service";
